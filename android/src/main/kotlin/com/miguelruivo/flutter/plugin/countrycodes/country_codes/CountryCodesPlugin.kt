8e3e3a09164496f1dedb3e9c7525e6e41a037fb2
Abdur Rafay Saleem <62943972+arafaysaleem@users.noreply.github.com>
2025-02-24 10:07:37 +0500

    Update CountryCodesPlugin.kt

--- android/src/main/kotlin/com/miguelruivo/flutter/plugin/countrycodes/country_codes/CountryCodesPlugin.kt
+++ android/src/main/kotlin/com/miguelruivo/flutter/plugin/countrycodes/country_codes/CountryCodesPlugin.kt
@@ -1,44 +1,55 @@
 package com.miguelruivo.flutter.plugin.countrycodes.country_codes
 
-import android.R
-import java.util.Locale
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.util.Locale
 
-/** CountryCodesPlugin */
-public class CountryCodesPlugin: FlutterPlugin, MethodCallHandler {
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "country_codes")
-    channel.setMethodCallHandler(CountryCodesPlugin());
-  }
+class CountryCodesPlugin : FlutterPlugin, MethodCallHandler {
+    private var channel: MethodChannel? = null
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-
-    when (call.method) {
-        "getLocale" -> result.success(listOf(Locale.getDefault().language, Locale.getDefault().country, getLocalizedCountryNames(call.arguments as String?)))
-        "getRegion" -> result.success(Locale.getDefault().country)
-        "getLanguage" -> result.success(Locale.getDefault().language)
-        else -> result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, CHANNEL_NAME).apply {
+            setMethodCallHandler(this@CountryCodesPlugin)
+        }
     }
-  }
 
-  private fun getLocalizedCountryNames(localeTag: String?) : HashMap<String, String> {
-    var localizedCountries: HashMap<String,String> = HashMap()
-
-    val deviceCountry: String = Locale.getDefault().toLanguageTag();
-
-    for (countryCode in Locale.getISOCountries()) {
-      val locale = Locale(localeTag ?: deviceCountry,countryCode)
-      var countryName: String? = locale.getDisplayCountry(Locale.forLanguageTag(localeTag ?: deviceCountry))
-      localizedCountries[countryCode.toUpperCase()] = countryName ?: "";
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getLocale" -> {
+                val localeInfo = listOf(
+                    Locale.getDefault().language,
+                    Locale.getDefault().country,
+                    getLocalizedCountryNames(call.arguments as? String)
+                )
+                result.success(localeInfo)
+            }
+            "getRegion" -> result.success(Locale.getDefault().country)
+            "getLanguage" -> result.success(Locale.getDefault().language)
+            else -> result.notImplemented()
+        }
     }
-    return localizedCountries
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-  }
+    private fun getLocalizedCountryNames(localeTag: String?): Map<String, String> {
+        val deviceCountry = Locale.getDefault().toLanguageTag()
+        val targetLocale = Locale.forLanguageTag(localeTag ?: deviceCountry)
+        
+        return Locale.getISOCountries().associate { countryCode ->
+            val locale = Locale("", countryCode)
+            val countryName = locale.getDisplayCountry(targetLocale)
+            countryCode.uppercase() to (countryName.ifEmpty { "" })
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        channel = null
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "country_codes"
+    }
 }
