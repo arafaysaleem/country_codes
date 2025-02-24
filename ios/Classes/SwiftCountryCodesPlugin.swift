b61b6e81327f7f9cfe8edf4fa3711b2330e5fe64
Abdur Rafay Saleem <62943972+arafaysaleem@users.noreply.github.com>
2025-02-24 10:10:14 +0500

    Update SwiftCountryCodesPlugin.swift

--- ios/Classes/SwiftCountryCodesPlugin.swift
+++ ios/Classes/SwiftCountryCodesPlugin.swift
@@ -2,38 +2,50 @@ import Flutter
 import UIKit
 
 public class SwiftCountryCodesPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "country_codes", binaryMessenger: registrar.messenger())
-    let instance = SwiftCountryCodesPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    
-    switch call.method {
-    case "getRegion":
-        result(Locale.current.regionCode)
-        break
-    case "getLanguage":
-        result(Locale.current.languageCode)
-        break
-    case "getLocale":
-        result([Locale.current.languageCode as Any, Locale.current.regionCode as Any, getLocalizedCountryNames(localeTag: call.arguments as? String)])
-        break
-    default:
-        result(FlutterMethodNotImplemented);
+    // MARK: - Constants
+    private enum Constants {
+        static let channelName = "country_codes"
     }
-  }
     
-    func getLocalizedCountryNames(localeTag: String?) -> Dictionary<String,String> {
-        var localizedCountries:Dictionary<String,String> = [String: String]()
+    // MARK: - FlutterPlugin
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: Constants.channelName,
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = SwiftCountryCodesPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
     
-        for countryCode in NSLocale.isoCountryCodes {
-            let countryName: String? = NSLocale(localeIdentifier: localeTag ?? Locale.preferredLanguages[0]).displayName(forKey: .countryCode, value: countryCode)
-            localizedCountries[countryCode.uppercased()] = countryName ?? "";
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getRegion":
+            result(Locale.current.regionCode)
+            
+        case "getLanguage":
+            result(Locale.current.languageCode)
+            
+        case "getLocale":
+            let localeInfo: [Any] = [
+                Locale.current.languageCode as Any,
+                Locale.current.regionCode as Any,
+                getLocalizedCountryNames(localeTag: call.arguments as? String)
+            ]
+            result(localeInfo)
+            
+        default:
+            result(FlutterMethodNotImplemented)
         }
-        return localizedCountries
     }
     
- 
+    // MARK: - Private Methods
+    private func getLocalizedCountryNames(localeTag: String?) -> [String: String] {
+        let preferredLocale = localeTag ?? Locale.preferredLanguages[0]
+        let locale = NSLocale(localeIdentifier: preferredLocale)
+        
+        return Dictionary(uniqueKeysWithValues: NSLocale.isoCountryCodes.map { countryCode in
+            let countryName = locale.displayName(forKey: .countryCode, value: countryCode)
+            return (countryCode.uppercased(), countryName ?? "")
+        })
+    }
 }
